752c1dd80a375e8b1c6440ea26193289eab2def6
fnwiya <fnwiya@gmail.com>
2015-12-04 23:23:40 +0900

    update

--- js/myT.js
+++ js/myT.js
@@ -1,22 +1,16 @@
-        var fin=!1;
-        function foo(){
-            $('mark').eq(0).css({background:'magenta'});
-        }
-        $(function(){
-            $('#top').t({
-                speed:35,
-                speed_vary:true,
-                mistype:20,
-                typing:function(elem,chars_total,chars_left,_char){
-                    if(_char=='*')foo();
-                },
+$(function(){
+    $('#code').t({
+        speed:35,
+        speed_vary:true,
+		delay:1,
+        mistype:20,
+		locale:'en',
+		caret:true,
+		blink:false,
+        typing:function(){
+        },
 
-                fin:function(){
-                    if(fin==!1){
-                        fin=!!1; //avoids triggering after 'add' cmd
-                        window.setTimeout(function(){$('#t').t('add','Still here?');},2e4);
-                        window.setInterval(function(){$('#t').find('.t-caret').toggle();},5e2);
-                    }
-                }
-            });
-        });
+        fin:function(){
+        }
+    });
+});
