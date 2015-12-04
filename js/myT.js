6222948618d13d58351f7071a0ccfcd3011d9a75
fnwiya <fnwiya@gmail.com>
2015-12-04 22:16:28 +0900

    set t

--- js/myT.js
+++ js/myT.js
@@ -1,8 +1,6 @@
 $(function(){
-var elem = '#id';
 var content = 'It\'s a <del>rainy</del>sunny day in <mark class="pink">Miami, FL</mark>.';
-
-$(elem).t(content,{
+$('#id').t(content,{
 
 //Basic settings
 speed:75,          // typing speed (ms)
@@ -15,18 +13,14 @@ blink:false,       // blink; if TRUE, 10ms or N ms
 tag:'span',        // wrapper (.t-container/.t-caret)
 repeat:false,      // if TRUE, infinite or N times
 
-//Callbacks
-init:function(elem){},
-typing:function(elem,chars_total,chars_left,char){},
-fin:function(elem){}
 
 });});
 
 //Methods
-$(elem).t('add',content);         // adds content
-$(elem).t('pause',true);          // pauses typing (toggles if 2nd param omitted)
+$('#id').t('add',content);         // adds content
+$('#id').t('pause',true);          // pauses typing (toggles if 2nd param omitted)
 
 //Data/properties
-$(elem).data('t');                // TRUE if initialised
-$(elem).data('is_typing');        // (bool)
-$(elem).data('pause');            // (bool)
+$('#id').data('t');                // TRUE if initialised
+$('#id').data('is_typing');        // (bool)
+$('#id').data('pause');            // (bool)
