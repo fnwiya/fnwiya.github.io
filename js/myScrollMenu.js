b78ac02e3f4fe5f2bc734b0e86fcff1bd7a3c45e
fnwiya <fnwiya@gmail.com>
2015-12-05 19:39:52 +0900

    update

--- js/myScrollMenu.js
+++ js/myScrollMenu.js
@@ -1,25 +1,20 @@
 var setupOption = {
-    template: '<div class="menu-wrap"><div class="menu"><%= label %></div></div>',
+    template: '<div class="menu-wrap"><div class="menu"></div></div>',
     anchorSetup: [
         {
-            backgroundColor: "#E74C3C",
-            label: "Top"
+            backgroundColor: "#E74C3C"
         },
         {
-            backgroundColor: "#E67E22",
-            label: "About"
+            backgroundColor: "#E67E22"
         },
         {
-            backgroundColor: "#F1C40F",
-            label: "Favorite"
+            backgroundColor: "#F1C40F"
         },
         {
-            backgroundColor: "#2ECC71",
-            label: "Works"
+            backgroundColor: "#2ECC71"
         },
         {
-            backgroundColor: "#1ABC9C",
-            label: "Contact"
+            backgroundColor: "#1ABC9C"
         }]
 };
 var scrollMenu = ScrollMenu(setupOption);
