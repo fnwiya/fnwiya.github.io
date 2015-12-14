734c22c47a856f7e3c4e18b6d0464ba630141000
fnwiya <fnwiya@gmail.com>
2015-12-14 21:26:31 +0900

    update

--- js/myHoneycombs.js
+++ js/myHoneycombs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.honeycombs').honeycombs({
-        combWidth:180,  // width of the hexagon
+        combWidth:200,  // width of the hexagon
         margin: 0,      // spacing between hexagon
         threshold: 3    // hide placeholder hexagons when number of hexagons in a row is more than the threshold number
     });
