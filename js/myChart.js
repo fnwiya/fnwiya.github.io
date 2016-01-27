b1672f409b3f8a1464ec72c9e55d1014eb84bd07
fnwiya <fnwiya@gmail.com>
2016-01-27 19:00:32 +0900

    update

--- js/myChart.js
+++ js/myChart.js
@@ -2,7 +2,7 @@ $(function() {
     // チャートの枠組み
     var radarChartData = {
         // 項目
-        labels: ["道具へのこだわり", "アルコール耐性", "カフェイン摂取量", "甘党", "早起き"],
+        labels: ["画力", "アルコール耐性", "カフェイン摂取量", "甘党", "早起き"],
         datasets: [
             {
                 // 透明を使いたいのでRGBAで色を再現→rgba(xxx,xxx,xxx,0.5):透過度50%
@@ -11,7 +11,7 @@ $(function() {
                 pointColor: "#eff1f5",              // チャートの点の色
                 pointStrokeColor: "#eff1f5",        // 点を囲む線の色
                 // 各項目の値
-                data: [10,2,7,10,5]
+                data: [3,2,7,10,5]
             }
         ]
     };
